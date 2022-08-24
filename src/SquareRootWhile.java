f87a13b806e9177db775a2090e82ae66613c7314
Kayla Holmes <Kayla.Holmes@cdpipelinedevshop>
2022-08-24 10:46:47 -0400

    "Finished Activities"

---
+++ src/SquareRootWhile.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class SquareRootWhile {
+    public static void main(String args[])
+    {
+        System.out.print("Type a non-negative integer: ");
+        Scanner console = new Scanner(System.in);
+        int number = console.nextInt();
+        while (number < 0) {
+            System.out.print("Invalid numeber, try again: ");
+            number = console.nextInt();
+        }
+        System.out.println("The square root of " +number+ " is " + Math.sqrt(number));
+
+    }
+
+}
